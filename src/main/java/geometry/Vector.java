b87adecb22c8913eb09e14abf9c04fb3887223f9
TimoKlingenhoefer <Timo.Klingenhoefer@web.de>
2018-12-04 00:27:51 +0100

    renamed class Vektor to Vector

--- src/main/java/geometry/Vector.java
+++ src/main/java/geometry/Vector.java
@@ -1,26 +1,26 @@
 package geometry;
 
-public class Vektor {
+public class Vector {
 
     public double a0, a1, a2;
 
-    public Vektor(double a0, double a1, double a2){
+    public Vector(double a0, double a1, double a2){
         this.a0 = a0;
         this.a1 = a1;
         this.a2 = a2;
     }
 
-    public Vektor normalize(){
+    public Vector normalize(){
         double length = Math.sqrt(Math.pow(Math.abs(a0), 2) + Math.pow(Math.abs(a1), 3) + Math.pow(Math.abs(a2), 2));
-        return new Vektor(a0/length, a1/length, a2/length);
+        return new Vector(a0/length, a1/length, a2/length);
     }
 
-    public static Vektor subtract(Vektor v0, Vektor v1){
-        return new Vektor(v0.a0 - v1.a0, v0.a1 - v1.a1, v0.a2 - v1.a2);
+    public static Vector subtract(Vector v0, Vector v1){
+        return new Vector(v0.a0 - v1.a0, v0.a1 - v1.a1, v0.a2 - v1.a2);
     }
 
-    public static Vektor crossproduct(Vektor v0, Vektor v1){
-        return new Vektor(
+    public static Vector crossproduct(Vector v0, Vector v1){
+        return new Vector(
                 v0.a1 * v1.a2 - v0.a2 * v1.a1,
                 v0.a2 * v1.a0 - v0.a0 * v1.a2,
                 v0.a0 * v1.a1 - v0.a1 * v1.a0);
